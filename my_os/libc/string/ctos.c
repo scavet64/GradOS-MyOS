d4415443c3e02dd3c0d5604dd6b481bfc5c86b01
Vincent Scavetta <scavet64@students.rowan.edu>
2018-11-27 00:05:08 -0500

    char to string implementation

---
+++ my_os/libc/string/ctos.c
@@ -0,0 +1,10 @@
+/**
+ * Translates a character into a string.
+ * Input - s: A two character array that will hold the character we want to convert to a string followed by the null character
+ * Input - c: the character we want to convert into a string
+ * Output - A pointer to the array that now holds the character string.
+ */
+char *ctos(char s[2], const char c) {
+    s[0] = c;
+    s[1] = '\0';
+}
