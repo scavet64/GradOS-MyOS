8567ced5bafe537aa30f333f346380f8eb4209b7
Vincent Scavetta <scavet64@students.rowan.edu>
2018-11-27 00:05:33 -0500

    concat strings implementation

---
+++ my_os/libc/string/strcat.c
@@ -0,0 +1,19 @@
+#include "../include/string.h" 
+
+/**
+ * Concatenate two strings together such that strcat(d,s) will give us the string d followed by the string s
+ * Input - d: The leftmost string in our concatenated string
+ * Input - s: The rightmost string in our concatenated string
+ * Output - The address of the two strings concatenated together
+ */
+char* strcat(char* d, const char* s) {
+    size_t firstLen = strlen(d);
+    size_t i;
+
+    for (i = 0 ; s[i] != '\0' ; i++) {
+        d[firstLen + i] = s[i];
+    }
+
+    d[firstLen + i] = '\0';
+    return d;
+}
