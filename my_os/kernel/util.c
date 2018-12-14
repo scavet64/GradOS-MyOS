274ef1c42ba751c7b45f7af47530807ba2075a7c
Vincent Scavetta <scavet64@students.rowan.edu>
2018-12-14 17:21:47 -0500

    Adding reverse, append and backspace to util

--- my_os/kernel/util.c
+++ my_os/kernel/util.c
@@ -1,3 +1,5 @@
+void reverse(char s[]);
+
 void memory_copy(char *source, char *dest, int nbytes) {
     int i;
     for (i = 0; i < nbytes; i++) {
@@ -19,5 +21,26 @@ void int_to_ascii(int n, char str[]) {
     if (sign < 0) str[i++] = '-';
     str[i] = '\0';
 
-    /* TODO: implement "reverse" */
+    reverse(str);
+}
+
+/* K&R */
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void append(char s[], char n) {
+    int len = strlen(s);
+    s[len] = n;
+    s[len+1] = '\0';
+}
+
+void backspace(char s[]) {
+    int len = strlen(s);
+    s[len-1] = '\0';
 }
--- my_os/kernel/util.h
+++ my_os/kernel/util.h
@@ -1,2 +1,4 @@
 void memory_copy(char *source, char *dest, int nbytes);
 void int_to_ascii(int n, char str[]);
+void append(char s[], char n);
+void backspace(char s[]);
